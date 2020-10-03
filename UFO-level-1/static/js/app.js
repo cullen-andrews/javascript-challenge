fc13261d5f5d8d822ff3424b895856e76efe201e
Cullen Andrews <66756039+cullen-andrews@users.noreply.github.com>
2020-10-02 18:17:46 -0700

    date filter achieved

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,40 +1,11 @@
-// from data.js
-var tableData = data;
 
-// Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(sighting => console.log(sighting));
+let tbody = d3.select("tbody");
 
-// Get a reference to the table body (Activity Evr_D3_Table)
-const tbody = d3.select("tbody");
+let ufos = data,
+  button = d3.select('#filter-btn'),
+  form = d3.select('#form')
 
-// Step 2:  Use d3 to append one table row `tr` for each weather report object
-// Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(sighting => {
-//   console.log(sighting);
-//   let row = tbody.append("tr");
-// });
-
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(sighting => {
-//   let row = tbody.append("tr");
-//   console.log(Object.values(sighting))
-//   console.log(Object.entries(sighting))
-// });
-
-// // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// data.forEach(sighting => {
-//   let row = tbody.append("tr");
-//   Object.values(sighting).forEach(value => {
-//     console.log(value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     let cell = row.append("td");
-//   });
-// });
-
-// // Step 5: Use d3 to update each cell's text with
-// // weather report values (weekday, date, high, low)
-data.forEach(sighting => {
+ufos.forEach(sighting => {
   let row = tbody.append("tr");
   Object.values(sighting).forEach(value => {
     // Append a cell to the row for each value
@@ -45,4 +16,75 @@ data.forEach(sighting => {
 });
 
 
-// console.log(data)
+/////////////////////////////////////
+
+const runEnter = () => {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    // Get the value property of the input element
+    let inputElement = d3.select("#datetime"), 
+        inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(ufos);
+  
+    let filteredData = ufos.filter(sighting => sighting.datetime === inputValue);
+  
+    console.log(filteredData);
+
+    // remove sightings from the table
+    tbody.html("")
+
+    filteredData.forEach(sighting => {
+        let row = tbody.append("tr");
+        Object.values(sighting).forEach(value => {
+          // Append a cell to the row for each value
+          // in the weather report object
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
+
+
+
+  
+    // // BONUS: Calculate summary statistics for the age field of the filtered data
+  
+    // // First, create an array with just the age values
+    // let ages = filteredData.map(person => person.age);
+  
+    // // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
+    // let mean = math.mean(ages), 
+    //     median = math.median(ages),
+    //     mode = math.mode(ages),
+    //     variance = math.var(ages),
+    //     standardDeviation = math.std(ages);
+  
+    // // Then, select the unordered list element by class name
+    // let list = d3.select(".summary");
+  
+    // remove any children from the list
+    // list.html("");
+  
+    // // append stats to the list
+    // list.append("li").text(`Mean: ${mean}`);
+    // list.append("li").text(`Median: ${median}`);
+    // list.append("li").text(`Mode: ${mode}`);
+    // list.append("li").text(`Variance: ${variance}`);
+    // list.append("li").text(`Standard Deviation: ${standardDeviation}`);
+  
+    // // var stats = [["mean", mean], ["median", median], ["mode", mode]];
+    // // stats.forEach(stat => list.append("li").text(`${stat[0]}: ${stat[1]}`));
+  
+    // // var stats = {Mean: mean, Median: median, Mode: mode, Variance: variance};
+    // // Object.entries(stats).forEach(([stat, value]) => list.append("li").text(`${stat}: ${value}`));
+  };
+  
+  // Create event handlers 
+  button.on("click", runEnter);
+  form.on("submit",runEnter);
+  
